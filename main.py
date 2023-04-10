b26ad3c85b86692b1fb37e5092116504111420c8
Diana <diana.lania@interia.pl>
2023-04-11 00:17:18 +0200

    Commit2

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpeg")
 
 with col2:
     st.title("Diana Lania")
     content = """
-    Hi, I am Diana! I am a Python programmer. I have already gianed extensive experience in Python web applications development.
-    Please have a look at my most recent projects.    
+    Hi, I am Diana! I am a Python programmer. I have already gained extensive experience in Python web applications development. 
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!  
+"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df.iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
